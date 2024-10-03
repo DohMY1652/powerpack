068e8c37d0db08774b12042a45ad3a447b242d6b
Myeongyun Doh <ehauddbs@g.skku.edu>
2024-10-03 21:13:55 +0900

    change yaml file as input

---
+++ include/DatabaseConfig.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <yaml-cpp/yaml.h>
+#include <vector>
+
+class DatabaseConfig {
+    public:
+
+        DatabaseConfig(const YAML::Node& config);
+        ~DatabaseConfig();
+        int get_n_pos_channel();
+        int get_n_neg_Channel();
+        std::vector<double> get_pos_pid_gains();
+        std::vector<double> get_neg_pid_gains();
+
+
+    private:
+        YAML::Node config;
+        int n_pos_channel;
+        int n_neg_channel;
+        std::vector<double> pos_pid_gains;
+        std::vector<double> neg_pid_gains;
+
+
+};
---
+++ src/DatabaseConfig.cpp
@@ -0,0 +1,38 @@
+#include "DatabaseConfig.h"
+
+DatabaseConfig::DatabaseConfig(const YAML::Node& config) : 
+    config(config) {
+        n_pos_channel = config["positive_channel"]["n_channel"].as<int>();
+        n_neg_channel = config["negative_channel"]["n_channel"].as<int>();
+        // for (const auto& gain : config["pos_pid_gains"]) {
+        //     pos_pid_gains.push_back(gain.begin()->second.as<double>());
+        // }
+        for (const auto& gain : config["pos_pid_gains"]) {
+                pos_pid_gains.push_back(gain.second.as<double>());
+            }
+       for (const auto& gain : config["neg_pid_gains"]) {
+                neg_pid_gains.push_back(gain.second.as<double>());
+            }
+        // pos_pid_gains = config["pos_pid_gains"].as<std::vector<double>>();
+        // neg_pid_gains = config["neg_pid_gains"].as<std::vector<double>>();
+    }
+
+DatabaseConfig::~DatabaseConfig() {
+
+}
+
+int DatabaseConfig::get_n_pos_channel() {
+    return n_pos_channel;
+}
+
+int DatabaseConfig::get_n_neg_Channel() {
+    return n_neg_channel;
+}
+
+std::vector<double> DatabaseConfig::get_pos_pid_gains() {
+    return pos_pid_gains;
+}
+
+std::vector<double> DatabaseConfig::get_neg_pid_gains() {
+    return neg_pid_gains;
+}
